2747c56dba5b908d426f83743fdf413938c3ca16
Mario García Alarcón <mariogarciaalarcon@gmail.com>
2024-04-11 16:29:37 +0200

    actualizado framework 2

---
+++ PHP/framework/Controllers/Estudiantes.php
@@ -0,0 +1,14 @@
+<?php 
+
+/*class ... extends Controller {
+    index() {
+        return $this->view('home', [
+            'title' => 'Home',
+            'description' => 'lalalala'
+        ]);
+    }
+}
+
+*/
+
+?>
